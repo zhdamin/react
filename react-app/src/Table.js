1cd554d4f3f8ea2ebf46b0cfd03705b779b65e25
zhdamin <zhongdaminy@163.com>
2020-06-22 20:02:01 +0800

    实现读取出工作，读取姓名有问题

--- react-app/src/Table.js
+++ react-app/src/Table.js
@@ -1,20 +1,28 @@
 import React,{Component} from 'react'
 
-const TableBody = () =>{
-    return <Table />
+const TableBody = props =>{
+    // return <tbody />
+    const rows = props.characterData.map((row,index) => {
+        return(
+            <tr key={index}>
+                <td>{row.Name}</td>
+                <td>{row.Job}</td>
+            </tr>
+        )
+    })
+
+return <tbody>{rows}</tbody>
 }
 
 
 const TableHeader = () => {
     return(
-        <table>
             <thead>
                 <tr>
                     <th>Name</th>
                     <th>Job</th>
                 </tr>
             </thead>
-        </table>
     )
 }
 
