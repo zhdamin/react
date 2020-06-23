4e2cbad7ee2b8f76b63a927f6282e5f33c48c868
zhdamin <zhongdaminy@163.com>
2020-06-23 19:41:14 +0800

    跟着学习视频做的react项目

---
+++ learn-react/src/index.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+//实现页面时刻的显示
+// function clock(){
+//   const time = new Date().toLocaleTimeString();
+//   const element = (
+//     <div>
+//       <h1>现在的时间是 {time} </h1>
+//     </div>
+//   )
+
+//   const root = document.querySelector('#root')
+//   ReactDOM.render(element,root)
+
+  
+// }
+
+// clock()
+
+// setInterval(clock,1000)
+
+
+//react函数式组件
+// function Clock(props){
+//   return(
+//     <div>
+//       <h1>现在的时间{props.date.toLocaleTimeString()}</h1>
+//     </div>
+//   )
+// }
+
+
+// function run(){
+//   ReactDOM.render(
+//     <Clock date = {new Date()} />,
+//     document.querySelector('#root')
+//   )
+// }
+
+
+// setInterval(run,1000)
+
+// const time =new Date().toLocaleTimeString()
+
+// const str = '当前的时间是：'
+
+// const element = (
+//   <div>
+//     <h1>hello world</h1>
+//     <h2>{str + time}</h2>
+//   </div>
+// )
+
+// const man = "发热"
+
+// const element2 = (
+//   <div>
+//     <h1>今天是否隔离</h1>
+//     <h2>{man == "发热"? <button>隔离</button>:"居家"}</h2>
+//   </div>
+// )
+
+const element4 = (
+  <div>
+    <span>aiya</span>
+    <span>haha</span>
+  </div>
+)
+
+const man = '正常'
+const element3 = (
+  <div>
+    <h1>今天是否隔离</h1>
+    <h2>{man == "发热"? <button>隔离</button>:element4}</h2>
+  </div>
+)
+
+
+ReactDOM.render(
+  element3,
+  document.getElementById("root")
+)
