cfcf159ebe0f97295cf3b121236e106a8488c4b6
zhdamin <zhongdaminy@163.com>
2020-06-29 19:44:18 +0800

    看react视频学习

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,27 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>ayufgyfglsa</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//history模式/后端匹配使用
+import {BrowserRouter as Route,Router, Link} from 'react-router-dom'
+
+function Home(){
+  return(
+    <div>
+      <h1>首页</h1>
     </div>
-  );
+  )
 }
+
+function Me(){
+  return(
+    <div>
+      <h1>个人中心</h1>
+    </div>
+  )
+}
+
+function Product(){
+  return(
+    <div>
+      <h1>产品页面</h1>
+    </div>
+  )
+}
+
+class App extends React.Component{
+  render(){
+    return(
+      <div id="app">
+        <div>
+          所有页面都显示的普通内容
+        </div>
+
+        <Router>
+          <Route path="/" exact component={()=>(<div>首页</div>)}></Route>
+          <Route path="/me" component={()=>(<div>me</div>)}></Route>
+          <Route path="/product" component={()=>(<div>product</div>)}></Route>
+        </Router>
+        <Router basename="/admin">
+          <div className="nav">
+            <Link to="/">Home</Link>
+            <Link to="/product">Product</Link>
+            <Link to="/me">个人中心</Link>
+          </div>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/product" component={Product}></Route>
+          <Route path="/me" component={Me}></Route>
+        </Router>
+      </div>
+    )
+  }
+}
+
 
 export default App;
--- learn-react/src/index.js
+++ learn-react/src/index.js
@@ -1,90 +1,9 @@
 import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-
-class ComLife extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      mes:'hello'
-    }
-    console.log('constructor构造函数')
-  }
-  componentWillMount(){
-    console.log('componentWillMount将要渲染')
-  }
-  componentDidMount(){
-    console.log('componentDidMount组件渲染完毕')
-  }
-  componentWillReceiveProps(){
-    console.log('componentWillReceiveProps组件将要接受新的状态')
-  }
-  shouldComponentUpdate(){
-    //如果希望更新，就返回为真，不希望更新就返回false
-    console.log(this.state.mes)
-    console.log('进行判断是否要更新内容')
-    if(this.state.mes === 'hello'){
-      return true
-    } else{
-      return false
-    }
-  }
-  componentWillUpdate(){
-    console.log('componentWillUpdate组件将要更新')
-  }
-  componentDidUpdate(){
-    console.log('componentDidUpdate组件更新完毕')
-  }
-  componentWillUnmount(){
-    console.log('componentWillUnmount移除')
-  }
-  render(){
-    console.log('render渲染函数')
-    return(
-      <div>
-        <h1>
-          {this.state.mes}
-        </h1>
-        <button onClick={this.changeMsg}>组件更新</button>
-      </div>
-    )
-  }
-
-  changeMsg = ()=>{
-    this.setState({
-      mes:'yayayyaya'
-    })
-  }
-}
-
-class ParentCom extends React.Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      isShow:true
-    }
-  }
-  render(){
-    if(this.state.isShow){
-      return (
-        <div>
-          <button onClick={this.removeCom}>移除comlife</button>,
-          <ComLife />
-        </div>
-      )
-    }else{
-      return <h1>将comlife移除</h1>
-    }
-  } 
-
-  removeCom = () => {
-    this.setState({
-      isShow:false
-    })
-  }
-}
-
+import ReactDOM from 'react-dom';
+import App from './App'
 
 ReactDOM.render(
-  <ParentCom />,
-  document.querySelector('#root')
-)
+  <App></App>,
+  document.getElementById('root')
+  )
+
